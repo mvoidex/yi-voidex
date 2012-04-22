2fd6d5e6c5d450a9f0f3c0ff5aa119ab9a5c381d
Alexandr Ruchkin <voidex.org@gmail.com>
2012-04-22 22:30:25 +0400

    Added config module

---
+++ src/Yi/Config/Users/VoidEx.hs
@@ -0,0 +1,14 @@
+module Yi.Config.Users.VoidEx (
+    voidexConfig
+    ) where
+
+import Yi.Prelude
+import Prelude ()
+
+import Yi
+
+import Yi.Keymap
+
+-- | My config
+voidexConfig :: Config
+voidexConfig = undefined
